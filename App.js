2c8c4764658216cd9588e7744f6da5ac9a7f9915
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2019-05-01 11:19:41 +0200

    created Decks component

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import AddEntry from './components/AddEntry';
+import Decks from './components/Decks'
 import SettingsScreen from './components/SettingsScreen';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -15,7 +16,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View>
-          <AddEntry />
+          <Decks />
         </View>
       </Provider>
     )
