8f23c8228b5afbf8549752fa4e55bb2ef3c0139d
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2019-05-01 12:23:15 +0200

    before wee try to add navigation

--- components/Decks.js
+++ components/Decks.js
@@ -1,16 +1,26 @@
-import React, {Component} from 'react'
-import {View, Text} from 'react-native'
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { connect } from 'react-redux'
 
-class Decks extends Component{
-  render(){
+class Decks extends Component {
+  render() {
     return (
-      <View style={{paddingTop: 20}}>
+      <View style={{ paddingTop: 20 }}>
         <Text>
           Hello from Decks component
+        </Text>
+        <Text>Entries: 
+          {this.props.entrie}
         </Text>
       </View>
     )
   }
 }
 
-export default Decks
+function mapStateToProps({ entries }) {
+  return {
+    entries: Object.keys(entries)
+  }
+}
+
+export default connect(mapStateToProps)(Decks)
---
+++ components/Hello.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+class Hello extends Component {
+  render() {
+    return (
+      <View style={{ paddingTop: 20 }}>
+        <Text>
+          Hello from Decks component
+        </Text>
+      </View>
+    )
+  }
+}
