bf205b2d70e09a9ec83faadc3dc36295f04a6bde
Bogdan Oloeriu <bogdan.oloeriu@gmail.com>
2019-05-08 01:08:05 +0200

    codded the notifications

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View , AsyncStorage} from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
 import { white } from './colors'
+import {Notifications, Permissions} from 'expo'
+
+export const NOTIFICATION_KEY = 'UdaciCards:notifications'
 
 export function getDeckMetaInfo(metaInfo) {
   const info = {
@@ -28,3 +31,51 @@ export function getDeckMetaInfo(metaInfo) {
 export function generateUID() {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
 }
+
+export function createNotification(){
+  return {
+    title:'Time to study',
+    body: "Don't forget to study today!",
+    ios:{
+      sound:true
+    },
+    android:{
+      sound:true,
+      priority:'high',
+      sticky:false,
+      vibrate:true
+    }
+  }
+}
+
+export function setLocalNotification(){
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null){
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then((status) => {
+            if (status === 'granted'){
+              Notifications.cancelAllScheduledNotificationsAsync()
+              let today = new Date()
+              today.setHours(20)
+              today.setMinutes(0)
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time:today,
+                  repeat:'day'
+                }
+              )
+              AsyncStorage.setItem(NOTIFICATION_KEY,JSON.stringify(ture))
+            }
+          })
+      }
+    })
+}
+
+export function clearLocalNotification(){
+  return AsyncStorage.removeItem(NOTIFICATION_KEY)
+   .then(Notifications.cancelAllScheduledNotificationsAsync())
+}
+
